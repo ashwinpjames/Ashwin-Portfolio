f2aa25aed4a5632cd916493510ef3ce20688ce14
Ashwin James <ashwinjames2720@gmail.com>
2026-08-12 12:31:05 +0400

    refactor: establish application routes for migration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout.jsx'
 import Home from './pages/Home.jsx'
 import PlaceholderPage from './pages/PlaceholderPage.jsx'
+
+const placeholderRoutes = [
+  '/about',
+  '/resume',
+  '/services',
+  '/case-studies',
+  '/case-studies/crm-sales-qualified-lead',
+  '/resources',
+  '/resources/campaign-budget-calculator',
+  '/blog',
+  '/contact',
+]
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        {placeholderRoutes.map((path) => (
+          <Route key={path} path={path} element={<PlaceholderPage />} />
+        ))}
         <Route path="*" element={<PlaceholderPage />} />
       </Route>
     </Routes>
