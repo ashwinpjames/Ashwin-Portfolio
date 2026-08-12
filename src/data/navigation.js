31b6f10ae88ca2a615c10c8851ca618371fbb6a2
Ashwin James <ashwinjames2720@gmail.com>
2026-08-12 12:25:24 +0400

    feat: centralize navigation data

---
+++ src/data/navigation.js
@@ -0,0 +1,10 @@
+export const navigation = [
+  { label: 'Home', path: '/' },
+  { label: 'About', path: '/about' },
+  { label: 'Resume', path: '/resume' },
+  { label: 'Services', path: '/services' },
+  { label: 'Case Studies', path: '/case-studies' },
+  { label: 'Resources', path: '/resources' },
+  { label: 'Blogs', path: '/blog' },
+  { label: 'Contact', path: '/contact' },
+]
