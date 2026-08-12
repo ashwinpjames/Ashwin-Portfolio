5e4e06e18eaf859e3338de77a57ac37342f23d57
Ashwin James <ashwinjames2720@gmail.com>
2026-08-12 14:36:10 +0400

    feat: complete homepage section composition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,19 @@
 import Hero from '../components/home/Hero.jsx'
 import PlatformMarquee from '../components/home/PlatformMarquee.jsx'
+import ProblemSection from '../components/home/ProblemSection.jsx'
+import BestFit from '../components/home/BestFit.jsx'
 import Results from '../components/home/Results.jsx'
 import AboutPreview from '../components/home/AboutPreview.jsx'
+import Approach from '../components/home/Approach.jsx'
 import ServicesPreview from '../components/home/ServicesPreview.jsx'
 import Process from '../components/home/Process.jsx'
 import CaseStudiesPreview from '../components/home/CaseStudiesPreview.jsx'
+import Testimonials from '../components/home/Testimonials.jsx'
+import FAQ from '../components/home/FAQ.jsx'
+import Insights from '../components/home/Insights.jsx'
+import ResourceLead from '../components/home/ResourceLead.jsx'
 import HomeCTA from '../components/home/HomeCTA.jsx'
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <PlatformMarquee />
-      <Results />
-      <AboutPreview />
-      <ServicesPreview />
-      <Process />
-      <CaseStudiesPreview />
-      <HomeCTA />
-    </main>
-  )
+  return <main><Hero /><PlatformMarquee /><ProblemSection /><BestFit /><Results /><AboutPreview /><Approach /><ServicesPreview /><Process /><CaseStudiesPreview /><Testimonials /><FAQ /><Insights /><ResourceLead /><HomeCTA /></main>
 }
