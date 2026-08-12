af51231d7bf69a0dd8a10e1abba7737cc5a2b74b
Ashwin James <ashwinjames2720@gmail.com>
2026-08-12 15:17:31 +0400

    refactor: centralize route metadata in layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,14 +2,8 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/layout/Header.jsx'
 import Footer from '../components/layout/Footer.jsx'
 import MobileActionBar from '../components/layout/MobileActionBar.jsx'
+import RouteMeta from '../components/layout/RouteMeta.jsx'
 
 export default function MainLayout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <MobileActionBar />
-      <Footer />
-    </>
-  )
+  return <><RouteMeta /><a className="skip-link" href="#main">Skip to content</a><Header /><Outlet /><MobileActionBar /><Footer /></>
 }
