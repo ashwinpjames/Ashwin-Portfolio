40b88dbcc2d7c6925b26391e21a99e752d640e33
Ashwin James <ashwinjames2720@gmail.com>
2026-08-12 15:14:42 +0400

    feat: route Contact page to React implementation

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,9 @@ import CrmCaseStudy from './pages/CrmCaseStudy.jsx'
 import Resources from './pages/Resources.jsx'
 import CampaignBudgetCalculator from './pages/CampaignBudgetCalculator.jsx'
 import Blog from './pages/Blog.jsx'
+import Contact from './pages/Contact.jsx'
 import PlaceholderPage from './pages/PlaceholderPage.jsx'
 
-const placeholderRoutes = ['/contact']
-
 export default function App() {
-  return <Routes><Route element={<MainLayout />}><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/resume" element={<Resume />} /><Route path="/services" element={<Services />} /><Route path="/case-studies" element={<CaseStudies />} /><Route path="/case-studies/crm-sales-qualified-lead" element={<CrmCaseStudy />} /><Route path="/resources" element={<Resources />} /><Route path="/resources/campaign-budget-calculator" element={<CampaignBudgetCalculator />} /><Route path="/blog" element={<Blog />} />{placeholderRoutes.map((path) => <Route key={path} path={path} element={<PlaceholderPage />} />)}<Route path="*" element={<PlaceholderPage />} /></Route></Routes>
+  return <Routes><Route element={<MainLayout />}><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/resume" element={<Resume />} /><Route path="/services" element={<Services />} /><Route path="/case-studies" element={<CaseStudies />} /><Route path="/case-studies/crm-sales-qualified-lead" element={<CrmCaseStudy />} /><Route path="/resources" element={<Resources />} /><Route path="/resources/campaign-budget-calculator" element={<CampaignBudgetCalculator />} /><Route path="/blog" element={<Blog />} /><Route path="/contact" element={<Contact />} /><Route path="*" element={<PlaceholderPage />} /></Route></Routes>
 }
