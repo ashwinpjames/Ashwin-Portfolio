a6f4b2e5d91f5dca001d76287ed41899123dc490
Ashwin James <ashwinjames2720@gmail.com>
2026-08-17 14:17:42 +0400

    style: load insights section styles

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import './styles/case-studies.css'
 import './styles/resources.css'
 import './styles/blog.css'
 import './styles/contact.css'
+import './styles/insights.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
