42810783b0c714cac7dee6dd253b8c606250db3a
Ashwin James <ashwinjames2720@gmail.com>
2026-08-17 12:06:36 +0400

    feat: mount cursor glow globally

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,8 @@ import Header from '../components/layout/Header.jsx'
 import Footer from '../components/layout/Footer.jsx'
 import MobileActionBar from '../components/layout/MobileActionBar.jsx'
 import RouteMeta from '../components/layout/RouteMeta.jsx'
+import CursorGlow from '../components/layout/CursorGlow.jsx'
 
 export default function MainLayout() {
-  return <><RouteMeta /><a className="skip-link" href="#page-content">Skip to content</a><Header /><div id="page-content"><Outlet /></div><MobileActionBar /><Footer /></>
+  return <><RouteMeta /><CursorGlow /><a className="skip-link" href="#page-content">Skip to content</a><Header /><div id="page-content"><Outlet /></div><MobileActionBar /><Footer /></>
 }
