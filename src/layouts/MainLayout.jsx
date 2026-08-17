fa925c4e1c80c6df81b36d39c6d9452b6af609fe
Ashwin James <ashwinjames2720@gmail.com>
2026-08-17 14:11:25 +0400

    fix: reset page scroll on navigation

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,8 @@ import Footer from '../components/layout/Footer.jsx'
 import MobileActionBar from '../components/layout/MobileActionBar.jsx'
 import RouteMeta from '../components/layout/RouteMeta.jsx'
 import CursorGlow from '../components/layout/CursorGlow.jsx'
+import ScrollToTop from '../components/layout/ScrollToTop.jsx'
 
 export default function MainLayout() {
-  return <><RouteMeta /><CursorGlow /><a className="skip-link" href="#page-content">Skip to content</a><Header /><div id="page-content"><Outlet /></div><MobileActionBar /><Footer /></>
+  return <><RouteMeta /><ScrollToTop /><CursorGlow /><a className="skip-link" href="#page-content">Skip to content</a><Header /><div id="page-content"><Outlet /></div><MobileActionBar /><Footer /></>
 }
