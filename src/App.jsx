153510a5cd5cd994e9085bfef2c7a1eab3477449
Ashwin James <ashwinjames2720@gmail.com>
2026-08-18 14:21:57 +0400

    Add dynamic routes for individual service pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Resume from './pages/Resume.jsx'
 import Services from './pages/Services.jsx'
+import ServiceDetail from './pages/ServiceDetail.jsx'
 import CaseStudies from './pages/CaseStudies.jsx'
 import CrmCaseStudy from './pages/CrmCaseStudy.jsx'
 import Resources from './pages/Resources.jsx'
@@ -13,5 +14,5 @@ import Contact from './pages/Contact.jsx'
 import PlaceholderPage from './pages/PlaceholderPage.jsx'
 
 export default function App() {
-  return <Routes><Route element={<MainLayout />}><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/resume" element={<Resume />} /><Route path="/services" element={<Services />} /><Route path="/case-studies" element={<CaseStudies />} /><Route path="/case-studies/crm-sales-qualified-lead" element={<CrmCaseStudy />} /><Route path="/resources" element={<Resources />} /><Route path="/resources/campaign-budget-calculator" element={<CampaignBudgetCalculator />} /><Route path="/blog" element={<Blog />} /><Route path="/contact" element={<Contact />} /><Route path="*" element={<PlaceholderPage />} /></Route></Routes>
+  return <Routes><Route element={<MainLayout />}><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/resume" element={<Resume />} /><Route path="/services" element={<Services />} /><Route path="/services/:slug" element={<ServiceDetail />} /><Route path="/case-studies" element={<CaseStudies />} /><Route path="/case-studies/crm-sales-qualified-lead" element={<CrmCaseStudy />} /><Route path="/resources" element={<Resources />} /><Route path="/resources/campaign-budget-calculator" element={<CampaignBudgetCalculator />} /><Route path="/blog" element={<Blog />} /><Route path="/contact" element={<Contact />} /><Route path="*" element={<PlaceholderPage />} /></Route></Routes>
 }
