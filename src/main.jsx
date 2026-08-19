2d9dd4a77c2effabdc626d105aa1a7f98cd79c81
Ashwin James <ashwinjames2720@gmail.com>
2026-08-19 11:39:12 +0400

    Load UTM builder styles

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './styles/globals.css'
+import './styles/home.css'
+import './styles/home-overrides.css'
+import './styles/about.css'
+import './styles/about-editorial.css'
+import './styles/resume.css'
+import './styles/services.css'
+import './styles/service-detail.css'
+import './styles/case-studies.css'
+import './styles/resources.css'
+import './styles/blog.css'
+import './styles/contact.css'
+import './styles/insights.css'
+import './styles/utm-builder.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
