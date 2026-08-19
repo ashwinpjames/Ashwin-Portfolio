1e305bee782fd52d1341f8f0f5aa04fe55f6fa90
Ashwin James <ashwinjames2720@gmail.com>
2026-08-19 11:38:55 +0400

    Add UTM builder route

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Resume from './pages/Resume.jsx'
+import Services from './pages/Services.jsx'
+import ServiceDetail from './pages/ServiceDetail.jsx'
+import MetaAdsService from './pages/MetaAdsService.jsx'
+import CaseStudies from './pages/CaseStudies.jsx'
+import CrmCaseStudy from './pages/CrmCaseStudy.jsx'
+import Resources from './pages/Resources.jsx'
+import CampaignBudgetCalculator from './pages/CampaignBudgetCalculator.jsx'
+import UTMBuilder from './pages/UTMBuilder.jsx'
+import Blog from './pages/Blog.jsx'
+import Contact from './pages/Contact.jsx'
+import PlaceholderPage from './pages/PlaceholderPage.jsx'
+
+export default function App() {
+  return <Routes><Route element={<MainLayout />}><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/resume" element={<Resume />} /><Route path="/services" element={<Services />} /><Route path="/services/meta-ads" element={<MetaAdsService />} /><Route path="/services/:slug" element={<ServiceDetail />} /><Route path="/case-studies" element={<CaseStudies />} /><Route path="/case-studies/crm-sales-qualified-lead" element={<CrmCaseStudy />} /><Route path="/resources" element={<Resources />} /><Route path="/resources/campaign-budget-calculator" element={<CampaignBudgetCalculator />} /><Route path="/resources/utm-builder" element={<UTMBuilder />} /><Route path="/blog" element={<Blog />} /><Route path="/contact" element={<Contact />} /><Route path="*" element={<PlaceholderPage />} /></Route></Routes>
+}
