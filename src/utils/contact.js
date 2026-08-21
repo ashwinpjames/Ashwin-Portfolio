eb6075149da10abf9f75160f1185b70731de4fff
Ashwin James <ashwinjames2720@gmail.com>
2026-08-21 12:12:43 +0400

    Fix contact details and WhatsApp link

--- src/utils/contact.js
+++ src/utils/contact.js
@@ -1,3 +1,3 @@
 export const emailAddress = 'ashwinjames2720@gmail.com'
 
-export const whatsappUrl = 'https://wa.me/97105227704142?text=Hello%20Ashwin%2C%20I%20would%20like%20to%20discuss%20generating%20more%20qualified%20leads%20for%20my%20business.'
+export const whatsappUrl = 'https://wa.me/971527704142?text=Hello%20Ashwin%2C%20I%20would%20like%20to%20discuss%20generating%20more%20qualified%20leads%20for%20my%20business.'
