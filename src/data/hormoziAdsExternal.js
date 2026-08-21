15034a89f7ce818d8853f28d8bd426c8f2b7763d
Ashwin James <ashwinjames2720@gmail.com>
2026-08-21 12:45:02 +0400

    Add Cloudinary hosted Hormozi ad references

---
+++ src/data/hormoziAdsExternal.js
@@ -0,0 +1,42 @@
+export const hormoziAds = [
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/729219646_1361541732534095_4249772417476555215_n.jpg',
+    alt: 'Meta ad calling out a specific business owner segment with a scaling roadmap',
+    caption: 'Business specific callout creative'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/728255013_1346405374332860_1824275421304752709_n.jpg',
+    alt: 'Meta ad calling out tree service business owners doing over one million dollars a year',
+    caption: 'Tree service owner callout'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/728286995_27130921449910684_5609014893862007582_n.jpg',
+    alt: 'Meta ad calling out an MSP or IT services company doing over one million dollars a year',
+    caption: 'MSP and IT services callout'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/727621678_1897357330954229_4862842828001176025_n.jpg',
+    alt: 'Meta ad calling out med spa business owners doing over one million dollars a year',
+    caption: 'Med spa callout'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/728555903_1574367474022409_5074681778585344385_n.jpg',
+    alt: 'Meta marketing workshop creative inviting business owners doing over one million dollars a year',
+    caption: 'Founder and workshop creative'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/767300881_2580826009014981_4034328188586406025_n.jpg',
+    alt: 'Meta ad calling out excavation company owners doing over one million dollars a year',
+    caption: 'Excavation company callout'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/729068644_1406105734773292_6554678395127265920_n.jpg',
+    alt: 'Meta ad calling out tax firm owners doing over one million dollars a year',
+    caption: 'Tax firm callout'
+  },
+  {
+    src: 'https://res.cloudinary.com/fo4xyppd/image/upload/v1787301755/728637095_1331948648305864_2491103331242992144_n.jpg',
+    alt: 'Meta ad calling out a veterinary clinic doing over one million dollars a year',
+    caption: 'Veterinary clinic callout'
+  }
+]
