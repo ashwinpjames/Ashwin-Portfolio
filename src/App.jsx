fff440e7b3a00a7e00133c6b3f1819fccfa457d0
Ashwin James <ashwinjames2720@gmail.com>
2026-08-24 15:18:02 +0400

    Add content angles blog route

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import LeadQualityBenchmarksBlog from './pages/LeadQualityBenchmarksBlog.jsx'
 import SeoAiSearchBlog from './pages/SeoAiSearchBlog.jsx'
 import MarketingConceptsBlog from './pages/MarketingConceptsBlog.jsx'
 import IncreaseAverageOrderValueBlog from './pages/IncreaseAverageOrderValueBlog.jsx'
+import ContentAnglesStrategyBlog from './pages/ContentAnglesStrategyBlog.jsx'
 import Resources from './pages/Resources.jsx'
 import CampaignBudgetCalculator from './pages/CampaignBudgetCalculator.jsx'
 import UTMBuilder from './pages/UTMBuilder.jsx'
@@ -45,6 +46,7 @@ export default function App() {
       <Route path="/resources/blog-prompt-architect" element={<BlogPromptArchitect />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/blog/increase-average-order-value" element={<IncreaseAverageOrderValueBlog />} />
+      <Route path="/blog/content-angles-strategy" element={<ContentAnglesStrategyBlog />} />
       <Route path="/blog/how-meta-ads-algorithm-works" element={<MetaAdsAlgorithmBlog />} />
       <Route path="/blog/hormozi-meta-ads-strategy" element={<HormoziMetaAdsStrategy />} />
       <Route path="/blog/more-leads-revenue-didnt-move" element={<LeadQualityBenchmarksBlog />} />
