02d607b5dc446f01a37949dff283bff0706364ce
Ashwin James <ashwinjames2720@gmail.com>
2026-09-05 12:49:48 +0400

    Remove Resume from navigation

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,7 +1,6 @@
 export const navigation = [
   { label: 'Home', path: '/' },
   { label: 'About', path: '/about' },
-  { label: 'Resume', path: '/resume' },
   { label: 'Services', path: '/services' },
   { label: 'Case Studies', path: '/case-studies' },
   { label: 'Resources', path: '/resources' },
