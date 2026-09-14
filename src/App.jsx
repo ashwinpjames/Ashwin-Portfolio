f869970b5866668b58feaef3a134d53b339306c9
Ashwin James <ashwinjames2720@gmail.com>
2026-09-14 23:43:22 +0400

    Add UAE strategy blog route

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ const PerformanceMarketingSpecialistBlog = lazyWithStyles(() => import('./pages/
 const PerformanceMarketingDubaiGuideBlog = lazyWithStyles(() => import('./pages/PerformanceMarketingDubaiGuideBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
 const FreelanceInvoiceGeneratorBlog = lazyWithStyles(() => import('./pages/FreelanceInvoiceGeneratorBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/freelance-marketing-consultant-blog.css')])
 const PerformanceMarketingVsDigitalMarketingBlog = lazyWithStyles(() => import('./pages/PerformanceMarketingVsDigitalMarketingBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
+const PerformanceMarketingStrategyUaeBlog = lazyWithStyles(() => import('./pages/PerformanceMarketingStrategyUaeBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
 const Resources = lazyWithStyles(() => import('./pages/Resources.jsx'), [() => import('./styles/resources.css')])
 const CampaignBudgetCalculator = lazyWithStyles(() => import('./pages/CampaignBudgetCalculator.jsx'), [() => import('./styles/resources.css')])
 const UTMBuilder = lazyWithStyles(() => import('./pages/UTMBuilder.jsx'), [() => import('./styles/resources.css'), () => import('./styles/utm-builder.css')])
@@ -95,6 +96,7 @@ export default function App() {
         <Route path="/blog/performance-marketing-dubai" element={<PerformanceMarketingDubaiGuideBlog />} />
         <Route path="/blog/free-invoice-generator-for-freelancers" element={<FreelanceInvoiceGeneratorBlog />} />
         <Route path="/blog/performance-marketing-vs-digital-marketing" element={<PerformanceMarketingVsDigitalMarketingBlog />} />
+        <Route path="/blog/performance-marketing-strategy-uae" element={<PerformanceMarketingStrategyUaeBlog />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<PlaceholderPage />} />
       </Route>
