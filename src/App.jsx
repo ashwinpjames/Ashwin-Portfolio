973590f4ada589281ed2b29a7e159778d50a8116
Ashwin James <ashwinjames2720@gmail.com>
2026-09-15 16:22:56 +0400

    Add marketing attribution blog route

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,7 @@ const PaidAdvertisingRoiBlog = lazyWithStyles(() => import('./pages/PaidAdvertis
 const CustomerAcquisitionCostVsCplBlog = lazyWithStyles(() => import('./pages/CustomerAcquisitionCostVsCplBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
 const RoasVsRoiBlog = lazyWithStyles(() => import('./pages/RoasVsRoiBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
 const CrmDataForPaidAdsBlog = lazyWithStyles(() => import('./pages/CrmDataForPaidAdsBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
+const MarketingAttributionBlog = lazyWithStyles(() => import('./pages/MarketingAttributionBlog.jsx'), [() => import('./styles/blog.css'), () => import('./styles/performance-marketing-specialist-blog.css')])
 const Resources = lazyWithStyles(() => import('./pages/Resources.jsx'), [() => import('./styles/resources.css')])
 const CampaignBudgetCalculator = lazyWithStyles(() => import('./pages/CampaignBudgetCalculator.jsx'), [() => import('./styles/resources.css')])
 const UTMBuilder = lazyWithStyles(() => import('./pages/UTMBuilder.jsx'), [() => import('./styles/resources.css'), () => import('./styles/utm-builder.css')])
@@ -139,6 +140,7 @@ export default function App() {
         <Route path="/blog/customer-acquisition-cost-vs-cpl" element={<CustomerAcquisitionCostVsCplBlog />} />
         <Route path="/blog/roas-vs-roi" element={<RoasVsRoiBlog />} />
         <Route path="/blog/crm-data-for-paid-ads" element={<CrmDataForPaidAdsBlog />} />
+        <Route path="/blog/why-attribution-matters-performance-marketing" element={<MarketingAttributionBlog />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<PlaceholderPage />} />
       </Route>
